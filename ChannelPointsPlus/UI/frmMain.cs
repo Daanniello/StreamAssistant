f31bbc3bb82522431d0b3e85ab7f47866b65e79f
Coolguy1260 <jackmario11@gmail.com>
2021-01-21 10:52:10 -0700

    fixing formatting in-browser
    fheruyfbehjwfwiufhwerifuyywrbehifiyehfuywefgheyhj

--- ChannelPointsPlus/UI/frmMain.cs
+++ ChannelPointsPlus/UI/frmMain.cs
@@ -142,7 +142,7 @@ namespace ChannelPointsPlus
             catch (IOException)
             {
                 logEvent.WaitOne();
-		        LogToTextFile(logMessage, logType);
+		LogToTextFile(logMessage, logType);
             }
         }
 
